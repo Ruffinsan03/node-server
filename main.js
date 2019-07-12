ff0716c9776f4093eb0b18315078d214c881aa89
Wilisa Ruffin <51328590+Ruffinsan03@users.noreply.github.com>
2019-07-11 23:01:39 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+//build a web server in one file and export and require it in to another file and be able to navigate to that file via the url and have it display.
+//parse a query string with your web server
+
+
+var http = required ('http');
+var server = http.createserver(function (req, res){;
+    res.writeHead(200,{'Content-Type': 'text/plain'});
+    res.end('How ya like me now');
+});
+
+server.listen(3000, '127.0.0.1'); 
+
+console.listen ('Now listening to port 8080');
+
+//Work in progress -- next step is getting the command line and console to speak to each other
+//Working more on end of month group presentation tonight and will tackle this exercise over the weekend 
+
+
+
+
+
+
+
+
+
+
+
